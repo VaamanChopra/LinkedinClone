bb3ee3890362d80a78e08260bb1a82cf361e0030
VaamanChopra <vaamanchopra@gmail.com>
2024-06-18 14:44:01 +0530

    Prevent Default while entering

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import "./InputOption"
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header />
-      {/* app body */}
-      <div className="app_body">
+      {!user ? (
+        <Login />
+        )  :(
+        <div className="app_body">
         <Sidebar/>
         <Feed />
       </div>
-      {/* feed */}
-      {/* Widgets */}
+      )
+      }
+      
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Login.css"
+
+function Login() {
+  return (
+    <div className='Login'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1280px-LinkedIn_Logo.svg.png" alt=""/>
+    </div>
+  )
+}
+
+export default Login
